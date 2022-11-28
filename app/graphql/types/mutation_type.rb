48798ba67321620182f701de5954a0db84121a53
Cami Meneses <kam_1095@live.com>
2022-11-27 22:05:57 -0500

    Add create method

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,34 @@
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
+    # First way not so good to create an Author
+
+    # mutation {
+    #   createAuthor(firstName: "Ernest", lastName: "Hemingway", yob: 1899, isAlive: false) {
+    #     id
+    #     fullName
+    #   }
+    # }
+
+    field :create_author, Types::AuthorType, null: false, description: "Create an Author" do
+      argument :first_name, String, required: false
+      argument :last_name, String, required: false
+      argument :yob, Int, required: false
+      argument :is_alive, Boolean, required: false
     end
+
+    def create_author(first_name:, last_name:, yob:, is_alive:)
+      Author.create!(first_name: first_name, last_name: last_name, yob: yob, is_alive: is_alive)
+    end
+
+    # Second way (Recommended)
+
+    # mutation {
+    #   createAnAuthor(firstName: "Ernest", lastName: "Hemingway", yob: 1899, isAlive: false) {
+    #     id
+    #     fullName
+    #   }
+    # }
+
+    field :create_an_author, Types::AuthorType, mutation: Mutations::CreateAnAuthor
   end
 end
